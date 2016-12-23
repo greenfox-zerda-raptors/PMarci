ad78eeab4d8ae45c3bfe81bcf1e8529d6329ef8a
Pósa Márton <posa.marci@gmail.com>
2016-12-23 12:27:01 +0100

    layout more or less satisfactory

--- AndroidStudioProjects/MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ AndroidStudioProjects/MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -4,15 +4,25 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static ArrayList<String> myStringArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        android.R.layout.simple_list_item_1, myStringArray);
+        ListView listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(adapter);
     }
 
     public void sendMessage(View view) {
