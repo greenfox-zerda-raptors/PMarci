f1d9a495b1a425b7a76795100da9573c95e2f582
Pósa Márton <posa.marci@gmail.com>
2016-12-23 13:47:02 +0100

    arrayadapter works

--- AndroidStudioProjects/MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ AndroidStudioProjects/MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -13,15 +13,17 @@ import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-    public static ArrayList<String> myStringArray = new ArrayList<>();
+    public ArrayList<String> myStringArray = new ArrayList<>();
+    public ListView listView;
+    public ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        adapter = new ArrayAdapter<String>(this,
         android.R.layout.simple_list_item_1, myStringArray);
-        ListView listView = (ListView) findViewById(R.id.listview);
+        listView = (ListView) findViewById(R.id.listview);
         listView.setAdapter(adapter);
     }
 
@@ -31,5 +33,11 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void addToList(View view) {
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        adapter.add(message);
     }
 }
